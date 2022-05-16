c36b03976892f84c1367341d22a71c2bb135e97f
Rajiece <103957046+Rajiece@users.noreply.github.com>
2022-05-16 23:23:52 +0530

    Update FibinacciSeries.java

--- FibinacciSeries.java
+++ FibinacciSeries.java
@@ -6,13 +6,13 @@ public static void main(String[] args) {
 	int firstNum=0; 
 	int secNum=1;
 	int sum=0;
-	System.out.print(firstNum+" ");
+	System.out.print(firstNum+" "+secNum);
     
 	for (int i =1;i<r;i++) {
 		sum=firstNum+secNum;
 		firstNum=secNum;
 		secNum=sum;
-	System.out.print(sum+" ");
+	System.out.print(" "+sum+" ");
 	}
 	
 }
